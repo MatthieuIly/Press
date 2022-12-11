75c516d53299069401f506c1bc3053fd0042610d
Matthieu Ily <sankokai2@hotmail.com>
2022-12-11 18:05:32 +0100

    feat: Building The Process Command

--- src/Console/ProcessCommand.php
+++ src/Console/ProcessCommand.php
@@ -2,7 +2,11 @@
 
 namespace Sankokai\Press\Console;
 
+use Illuminate\Support\Str;
 use Illuminate\Console\Command;
+use Sankokai\Press\Models\Post;
+use Sankokai\Press\PressFileParser;
+use Illuminate\Support\Facades\File;
 
 class ProcessCommand extends Command
 {
@@ -11,6 +15,21 @@ class ProcessCommand extends Command
     protected $description = 'Updates blog posts.';
 
     public function handle() {
-        $this->info('Hello');
+        // Fetch all posts
+        $files = File::files('blogs');
+
+        // Process each files
+        foreach ($files as $file) {
+            $post = (new PressFileParser($file->getPathname()))->getData();
+        }
+
+        // Persis to the DB
+        Post::Create([
+            'identifier' => Str::random(),
+            'slug' => Str::slug($post->title),
+            'title' => $post['title'],
+            'body' => $post['body'],
+            'extra' => $post['extra'] ?? [] ,
+        ]);
     }
 }
