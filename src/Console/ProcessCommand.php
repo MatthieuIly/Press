b785595bf56b3a7247d761d3f6883424f52324f2
Matthieu Ily <sankokai2@hotmail.com>
2022-12-11 17:51:18 +0100

    feat: Process Console Command

---
+++ src/Console/ProcessCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Sankokai\Press\Console;
+
+use Illuminate\Console\Command;
+
+class ProcessCommand extends Command
+{
+    protected $signature = 'press:process';
+
+    protected $description = 'Updates blog posts.';
+
+    public function handle() {
+        $this->info('Hello');
+    }
+}
